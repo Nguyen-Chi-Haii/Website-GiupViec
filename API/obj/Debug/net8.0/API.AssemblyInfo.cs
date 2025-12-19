c194245821a0666a9b7cf3e987263d4d2c5aac50
ChiHaiNguyen <Haiforwork2005@gmail.com>
2025-12-19 13:43:35 +0700

    bo sung controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc16004e10fc83c6fed1fffb194e0709bd4208b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8092ba0604d83b80ad5d0d40320b7f46745b4fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
